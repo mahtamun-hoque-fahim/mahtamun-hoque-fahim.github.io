25267b7924fcff58f2551fa606aaa8e6825cc048
Mahtamun Hoque Fahim <mahtamunhoquefahim@gmail.com>
2021-10-22 21:27:54 +0600

    no source code !

--- assets/web/app.js
+++ assets/web/app.js
@@ -1,6 +1,3 @@
-
-let element = document.querySelector('a');
-element.addEventListener("click" , function(){
-    element.style.border = ".5px solid red"
-}
-)
+document.addEventListener("contextmenu" , function(fahim){
+    fahim.preventDefault();
+  });
